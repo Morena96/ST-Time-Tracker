211310cb62a3ae3f4b48bd21a8e7564d60bf28d2
Dovlet Aydogdyyev <38159544+Morena96@users.noreply.github.com>
2025-02-18 11:13:07 +0500

    Add storage support for API key input

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,30 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Box, Inline, Text,Textfield,Button,Link } from '@forge/react';
+import ForgeReconciler, { Box, Inline, Text, Textfield, Button, Link } from '@forge/react';
 import { invoke } from '@forge/bridge';
+import { storage } from '@forge/api';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  }, []);
+
+  console.log("atabay");
+
+  const handleApiKeyChange = async (e) => {
+    await storage.set('apiKey', e.target.value);
+  };
+
   return (
     <>
       <Inline>
-      <Box padding='space.200'> </Box>
-      <Text>Enter your ScrumTeams API key:</Text>
+        <Box padding='space.200'> </Box>
+        <Text>Enter your ScrumTeams API key:</Text>
       </Inline>
       <Box padding='space.100'> </Box>
-      <Inline  alignBlock='center'>
-      <Box padding='space.200'> </Box>
-      <Textfield></Textfield>
-      <Box padding='space.100'> </Box>
-      <Button>Update</Button>
-      <Box padding='space.200'> </Box>
+      <Inline alignBlock='center'>
+        <Box padding='space.200'> </Box>
+        <Textfield onChange={handleApiKeyChange}></Textfield>
+        <Box padding='space.100'> </Box>
+        <Button>Update</Button>
+        <Box padding='space.200'> </Box>
       </Inline>
       <Box padding='space.100'> </Box>
       <Inline>
-      <Box padding='space.200'> </Box>
-      <Link href="https://atlassian.com" openNewTab={true}>Get API key</Link>
+        <Box padding='space.200'> </Box>
+        <Link href="https://atlassian.com" openNewTab={true}>Get API key</Link>
       </Inline>
     </>
   );
