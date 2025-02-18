3ed9f89fedfa193cf4675c44604d8dcd49dbdf7e
Dovlet Aydogdyyev <38159544+Morena96@users.noreply.github.com>
2025-02-18 09:43:01 +0500

    Add ScrumTeams API key input form and update manifest permissions

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
+import ForgeReconciler, { Box, Inline, Text,Textfield,Button,Link } from '@forge/react';
 import { invoke } from '@forge/bridge';
 
 const App = () => {
@@ -9,8 +9,23 @@ const App = () => {
   }, []);
   return (
     <>
-      <Text>Hello world!</Text>
-      <Text>{data ? data : 'Loading...'}</Text>
+      <Inline>
+      <Box padding='space.200'> </Box>
+      <Text>Enter your ScrumTeams API key:</Text>
+      </Inline>
+      <Box padding='space.100'> </Box>
+      <Inline  alignBlock='center'>
+      <Box padding='space.200'> </Box>
+      <Textfield></Textfield>
+      <Box padding='space.100'> </Box>
+      <Button>Update</Button>
+      <Box padding='space.200'> </Box>
+      </Inline>
+      <Box padding='space.100'> </Box>
+      <Inline>
+      <Box padding='space.200'> </Box>
+      <Link href="https://atlassian.com" openNewTab={true}>Get API key</Link>
+      </Inline>
     </>
   );
 };
